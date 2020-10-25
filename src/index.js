bbd5afcb9a02f671f120dd59001b5ab04973deac
PavelVasilev <vpnprint@gmail.com>
2020-10-25 19:21:11 +0300

    feat: correct test code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  while (true) {
+    let iter = 0;
+    bracketsConfig.forEach(item => {
+        if (str.includes(item[0] + item[1])) {
+            
+            str = str.replace(item[0] + item[1], '')
+            
+            iter++;
+        }
+    })
+    if (iter === 0) {
+        break;
+    }
+}
+if (str === '') {
+    return true;
+} else {
+    return false;
+}
 }
